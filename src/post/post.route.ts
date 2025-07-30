4a7ee25151350aa1885f86eeb85ec6506a56e6d0
AndrianinaH <rakotoandrianina@gmail.com>
2025-07-30 14:16:20 +0300

    list post with pagination

--- src/post/post.route.ts
+++ src/post/post.route.ts
@@ -1,9 +1,19 @@
 import { db } from "../drizzle/db.ts";
 import { Hono } from "hono";
+import { post } from "../drizzle/schema.ts";
+import { asc, desc } from "drizzle-orm";
 
 export const postRoute = new Hono();
 
 postRoute.get("/", async (c) => {
-  const posts = await db.query.post.findMany();
+  const { page = 1, pageSize = 10, orderBy = "createdAt:desc" } = c.req.query();
+
+  const [field, direction] = orderBy.split(":");
+
+  const posts = await db.query.post.findMany({
+    limit: pageSize,
+    offset: (page - 1) * pageSize,
+    orderBy: [direction === "asc" ? asc(post[field]) : desc(post[field])],
+  });
   return c.json(posts);
 });
