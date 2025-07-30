5768999919a59c9663471a47a3a962fa869c5c85
AndrianinaH <rakotoandrianina@gmail.com>
2025-07-30 22:04:47 +0300

    use authmiddleware

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
+// main.ts
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { postRoute } from "./src/post/post.route.ts";
 import { statsRoute } from "./src/stats/stats.route.ts";
 import { loginRoute } from "./src/login/login.route.ts";
+import { authMiddleware } from "./src/utils/auth-middleware.ts";
 
 const app = new Hono();
 
 app.use("/*", cors({ origin: "*" }));
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+// Routes publiques (aucune authent requise)
+app.get("/", (c) => c.text("Hello Hono!"));
+app.route("/login", loginRoute);
+
+// TOUTES les autres routes passent par authMiddleware
+app.use("/posts/*", authMiddleware);
+app.use("/stats/*", authMiddleware);
 
 app.route("/posts", postRoute);
 app.route("/stats", statsRoute);
-app.route("/login", loginRoute);
 
 Deno.serve(app.fetch);
